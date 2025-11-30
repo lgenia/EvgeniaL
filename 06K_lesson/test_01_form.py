55ab67df2e44a0ea84c0a056e6ccabcea3fc5d4b
lgenia <lgenia@mail.ru>
2025-11-30 19:40:40 +0500

    06k

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,70 +1,65 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_buttons():
     driver = webdriver.Edge()
-    driver.get(
-        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-    driver = WebDriverWait(7)
+    wait = WebDriverWait(driver, 7)
 
-    first_name = driver.find_element(
-        By.CSS_SELECTOR, "[name='first-name']")
+    first_name = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='first-name']")))
     first_name.send_keys('Иван')
     first_name.click()
 
-    last_name = driver.find_element(By.CSS_SELECTOR, "[name='last-name']")
+    last_name = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='last-name']")))
     last_name.send_keys('Петров')
     last_name.click()
 
-    address = driver.find_element(
-        By.CSS_SELECTOR, "[name='address']")
+    address = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='address']")))
     address.send_keys('Ленина, 55-3')
     address.click()
 
-    mail = driver.find_element(
-        By.CSS_SELECTOR, "[name='e-mail']")
+    mail = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='e-mail']")))
     mail.send_keys("test@skypro.com")
     mail.click()
 
-    phone = driver.find_element(
-        By.CSS_SELECTOR, "[name='phone']")
+    phone = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='phone']")))
     phone.send_keys("+7985899998787")
     phone.click()
 
-    city = driver.find_element(
-        By.CSS_SELECTOR, "[name='city']")
+    city = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='city']")))
     city.send_keys("Москва")
     city.click()
 
-    contry = driver.find_element(
-        By.CSS_SELECTOR, "[name='country']")
-    contry.send_keys("Россия")
-    contry.click()
+    country = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='country']")))
+    country.send_keys("Россия")
+    country.click()
 
-    job = driver.find_element(
-        By.CSS_SELECTOR, "[name='job-position']")
+    job = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='job-position']")))
     job.send_keys("QA")
     job.click()
 
-    company = driver.find_element(
-        By.CSS_SELECTOR, "[name='company']")
+    company = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[name='company']")))
     company.send_keys("SkyPro")
     company.click()
 
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
-    zip_code = driver.find_element(
-            By.ID, "zip-code").value_of_css_property("background-color")
-    assert zip_code == "rgba(248, 215, 218, 1)"
+    zip_code = wait.until(EC.presence_of_element_located((By.ID, "zip-code")))
+    background_color = zip_code.value_of_css_property("background-color")
+
+    assert background_color == "rgba(248, 215, 218, 1)"
 
     fields = ["first-name", "last-name", "address", "e-mail", "phone",
               "city", "country", "job-position", "company"]
+    
     for field in fields:
-        tabs = driver.find_element(
-            By.ID, field).value_of_css_property("background-color")
-        assert tabs == "rgba(209, 231, 221, 1)"
+        element = wait.until(EC.presence_of_element_located((By.ID, field)))
+        color = element.value_of_css_property("background-color")
+
+    assert color == "rgba(209, 231, 221, 1)"
 
     driver.quit()
